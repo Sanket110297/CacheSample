7b0b7b0528d0f971241f5dda04e7cb5a11d66b12
Sanket110297 <san110297@gmail.com>
2020-07-25 23:52:15 +0530

    Asynchronous methods added

--- src/main/java/com/test/controller/LocationController.java
+++ src/main/java/com/test/controller/LocationController.java
@@ -1,6 +1,9 @@
 package com.test.controller;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +14,18 @@ import com.test.service.LocationService;
 public class LocationController {
 
 	@Autowired
+	@Lazy
 	private LocationService locationService;
 
 	@GetMapping("/fetch/{cityName}")
 	public String getHereAPIresponse(@PathVariable String cityName) {
-		return locationService.getSpots(cityName);
+		try {
+			return locationService.getSpots(cityName);
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cityName;
 
 	}
 }
--- src/main/java/com/test/service/LocationService.java
+++ src/main/java/com/test/service/LocationService.java
@@ -2,10 +2,15 @@ package com.test.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,29 +26,34 @@ public class LocationService {
 	 * @Autowired CacheManager cache;
 	 */
 	@Autowired
+	@Lazy
 	LocationService service;
 	private static final RestTemplate restTemplate = new RestTemplate();;
 	private static final ObjectMapper mapper = new ObjectMapper();
 	private static final String apiKey = "";
 
-	@CacheEvict(allEntries = true, cacheNames = { "geoCode", "chargingSpots" })
+	@CacheEvict(allEntries = true, cacheNames = { "geoCode", "chargingSpots", "restaurants", "petrol-stations" })
 	@Scheduled(fixedRate = 20000)
 	public void clearCache() {
-		System.out.println("clearing");
 	}
 
-	public String getSpots(String cityName) {
+	public String getSpots(String cityName) throws InterruptedException, ExecutionException {
 		List<Float> geoCode = service.fetchGeoCodesByCityName(cityName);
-		List<Object> nearbyRestaurants = service.getNearByChargingSpots(geoCode);
-		System.out.println(nearbyRestaurants);
-		return "Hello";
+		CompletableFuture<List<Object>> nearbyRestaurants = service.getNearByRestaurants(geoCode);
+		CompletableFuture<List<Object>> nearbyChargingStations = service.getNearByChargingSpots(geoCode);
+		CompletableFuture<List<Object>> nearbyPetrolStations = service.getNearByPetrolStations(geoCode);
+//		CompletableFuture.allOf(nearbyRestaurants, nearbyChargingStations, nearbyPetrolStations);
+		System.out.println(nearbyRestaurants.get());
+		System.out.println(nearbyChargingStations.get());
+		System.out.println(nearbyPetrolStations.get());
+		return nearbyRestaurants.toString() + "-------------------------------------"
+				+ nearbyChargingStations.toString();
 	}
 
 	@Cacheable("geoCode")
 	public List<Float> fetchGeoCodesByCityName(String cityName) {
 		String url = "https://geocoder.ls.hereapi.com/6.2/geocode.json?searchtext=" + cityName + "&gen=9&apiKey="
 				+ apiKey;
-		System.out.println("From method fetchGeoCodesByCityName");
 		JsonNode returnFromAPI = getResponseFromThirdPartyAPI(url);
 		JsonNode geoCode = returnFromAPI.get("Response").get("View").get(0).get("Result").get(0).get("Location")
 				.get("DisplayPosition");
@@ -54,14 +64,56 @@ public class LocationService {
 	}
 
 	@Cacheable("chargingSpots")
-	public List<Object> getNearByChargingSpots(List<Float> geoCode) {
+	@Async
+	public CompletableFuture<List<Object>> getNearByChargingSpots(List<Float> geoCode) {
 		String url = "https://places.ls.hereapi.com/places/v1/discover/search?at=" + geoCode.get(0) + ","
-				+ geoCode.get(1) + "&q=restaurant&apiKey=" + apiKey;
+				+ geoCode.get(1) + "&q=charging-stations&apiKey=" + apiKey;
 		JsonNode returnFromAPI = getResponseFromThirdPartyAPI(url);
 		List<Object> items = new ArrayList<>();
-		returnFromAPI.get("result").get("items").forEach(item -> items.add(item));
-//		String response = returnFromAPI.toString();
-		return items;
+		returnFromAPI.get("results").get("items").forEach(item -> items.add(item.get("title")));
+		try {
+			System.out.println("2-sleeping-Cha");
+			Thread.sleep(5000);
+			System.out.println("2-DONE-sleeping-Cha");
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.err.println(22222);
+		return CompletableFuture.completedFuture(items);
+	}
+
+	@Cacheable("restaurants")
+//	@Async
+	public CompletableFuture<List<Object>> getNearByRestaurants(List<Float> geoCode) {
+		String url = "https://places.ls.hereapi.com/places/v1/discover/search?at=" + geoCode.get(0) + ","
+				+ geoCode.get(1) + "&q=restaurants&apiKey=" + apiKey;
+		JsonNode returnFromAPI = getResponseFromThirdPartyAPI(url);
+		List<Object> items = new ArrayList<>();
+		returnFromAPI.get("results").get("items").forEach(item -> items.add(item.get("title")));
+		try {
+			System.out.println("1-Sleeping-Rest");
+			Thread.sleep(10000);
+			System.out.println("1-Done-sleeping-Rest");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.err.println(1111);
+		return CompletableFuture.completedFuture(items);
+	}
+
+	@Cacheable("petrol-stations")
+//	@Async
+	public CompletableFuture<List<Object>> getNearByPetrolStations(List<Float> geoCode) {
+		String url = "https://places.ls.hereapi.com/places/v1/discover/search?at=" + geoCode.get(0) + ","
+				+ geoCode.get(1) + "&q=petrol-stations&apiKey=" + apiKey;
+		JsonNode returnFromAPI = getResponseFromThirdPartyAPI(url);
+		List<Object> items = new ArrayList<>();
+		returnFromAPI.get("results").get("items").forEach(item -> items.add(item.get("title")));
+		System.err.println(333333);
+		return CompletableFuture.completedFuture(items);
 	}
 
 	private JsonNode getResponseFromThirdPartyAPI(String url) {
